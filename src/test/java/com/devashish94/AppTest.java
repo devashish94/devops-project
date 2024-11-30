0675f3133ee5a5eb937784fd14be20bdc6051661
devashish94 <roy.devashish94@gmail.com>
2024-12-01 05:00:25 +0530

    more things

--- src/test/java/com/devashish94/AppTest.java
+++ src/test/java/com/devashish94/AppTest.java
@@ -9,13 +9,13 @@ class AppTest {
     @Test
     void testAdd() {
         App app = new App();
-        assertEquals(5, app.add(2, 3), "The addition of 2 and 3 should be 5");
+        assertEquals(5, app.add(2, 3), "The addition of 2 and 3 should be 5.");
     }
 
     // Test for the subtract method
     @Test
     void testSubtract() {
         App app = new App();
-        assertEquals(1, app.subtract(3, 2), "The subtraction of 3 and 2 should be 1");
+        assertEquals(1, app.subtract(3, 2), "The subtraction of 3 and 2 should be 1.");
     }
 }
