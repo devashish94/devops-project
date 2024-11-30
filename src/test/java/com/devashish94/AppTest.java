0465e235f3cc69139907601fa63386e1a255d87f
devashish94 <roy.devashish94@gmail.com>
2024-12-01 04:49:30 +0530

    update

--- src/test/java/com/devashish94/AppTest.java
+++ src/test/java/com/devashish94/AppTest.java
@@ -1,5 +1,21 @@
 package com.devashish94;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppTest {
 
+    // Test for the add method
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3), "The addition of 2 and 3 should be 5");
+    }
+
+    // Test for the subtract method
+    @Test
+    public void testSubtract() {
+        App app = new App();
+        assertEquals(1, app.subtract(3, 2), "The subtraction of 3 and 2 should be 1");
+    }
 }
